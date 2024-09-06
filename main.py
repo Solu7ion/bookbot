e980c92695830823e182230ab6968edce7e829ad
Solu7ion <armanid03.02@mail.ru>
2024-09-06 17:49:19 +0500

    Rename variables and simplify logic

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_word(text)
-    print(f"words: {num_words}")
-
-
-
-    lower_text = get_lower_words(text)
-    character_count = get_character_count(lower_text)
+    character_count = get_character_count(text)
     print(character_count)
 
 def get_book_text(path):
@@ -18,20 +13,15 @@ def get_num_word(text):
     words = text.split()
     return len(words)
 
-def get_lower_words(text):
-    lowered_text = text.lower()
-    return lowered_text
-
-def get_character_count(lower_text):
-    lower_string = lower_text.split() 
-    character_count = {}
-    for word in lower_string:
-        for char in word:
-            if char in character_count:
-                character_count[char] +=1
-            else:
-                character_count[char] = 1
-    return character_count
+def get_character_count(text): 
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] +=1
+        else:
+            chars[lowered] =1
+    return chars
 main()
 
 
